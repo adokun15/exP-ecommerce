3fe00b1d84670b599265b8c88c5f1e06ecbd7f58
adokun15 <amosdaniel2005@gmail.com>
2024-08-26 14:30:43 +0100

    Let get Started

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import "./index.css";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { Provider } from "react-redux";
+//import store from "./store";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  //<Provider store={store}>
+  <App />
+  //</Provider>
+);
